651e5549c6d810b6e96b52df3d0dff28f09244d0
Иван <ivan.shutov95@mail.ru>
2025-08-01 23:20:10 +0300

    Hw1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задача 1 ");
+        int[] first = new int[]{1, 2, 3};
+        float[] two = {1.57f, 7.654f, 9.986f};
+        int[] free = new int[10];
+    }
+}
+
